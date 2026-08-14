ef94553e1c05d8b98c104482bab0bd1145a5d691
Roadom <mondavidolarte@proton.me>
2026-08-14 12:02:05 +0800

    feat: test database setup

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,16 @@
 -- Create database if not exists
 CREATE DATABASE IF NOT EXISTS expense_system_development CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS expense_system_test CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+GRANT ALL PRIVILEGES
+ON expense_system_development.*
+TO 'expense_user'@'%';
+
+GRANT ALL PRIVILEGES
+ON expense_system_test.*
+TO 'expense_user'@'%';
+
+FLUSH PRIVILEGES;
 -- USE expense_system_development;
 
 -- -- Create categories table
